a87e704bff795f063471b582706c149830a316e1
nastialyash <user251@school22.rv.ua>
2024-09-07 21:33:31 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,24 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "\"Hello word!\"" << endl;
+	cout << "Bjarne Soustrap\\\n" << endl;
+
+
+	cout << "Hello word!\n" << endl;
+	cout << "\\\\DEnis Ritchi\\\\" << endl;
+
+    
+    cout << "To be, or not to be: that is the question:\n"<<endl;
+    cout << "Whether 'tis nobler in the mind to suffer\n"<<endl;
+    cout << "The slings and arrows of outrageous fortune,\n"<<endl;
+    cout << "Or to take arms against a sea of troubles,\n"<<endl;
+    cout << "And by opposing end them?\n"<<endl;
+    cout << "\n"<<endl;
+    cout << "William Shakespeare\n"<<endl;
+
+}
+
